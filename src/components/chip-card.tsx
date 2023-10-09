2009584f92dd5ef62f0c88f05611fc716208f064
MaximePeterburg <getmax4444@gmail.com>
2023-10-10 02:36:26 +0300

    added hover effect

--- src/components/chip-card.tsx
+++ src/components/chip-card.tsx
@@ -10,7 +10,7 @@ const ChipCard: FC<ChipCardProps> = ({ chip }) => {
     <li className="inline-block list-none">
       <label className="cursor-pointer">
         <input className="peer sr-only" type="checkbox" />
-        <div className="mx-0.5 my-1 rounded-xl bg-neutral-200 px-0.5 py-2.5 text-xs ring-1 ring-transparent peer-checked:ring-indigo-600 peer-checked:ring-offset-1">
+        <div className="mx-0.5 my-1 rounded-xl bg-neutral-300 px-0.5 py-2.5 text-xs ring-1 ring-transparent hover:bg-neutral-200  peer-checked:ring-indigo-600 peer-checked:ring-offset-1">
           Чипс {chip.id}
         </div>
       </label>
--- src/components/chips-collection.tsx
+++ src/components/chips-collection.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { CHIPS_DATA } from "../../data/CHIPS_DATA";
-import ChipContainer from "./chip-card";
+import ChipCard from "./chip-card";
 
 const ChipsCollection = () => {
   const [isMobileDropdownOpen, setIsMobileDropdownOpen] = useState(false);
@@ -8,10 +8,12 @@ const ChipsCollection = () => {
 
   const handleMobileClick = () => {
     setIsMobileDropdownOpen(!isMobileDropdownOpen);
+    setIsTabletDropdownOpen(!isMobileDropdownOpen);
   };
 
   const handleTabletClick = () => {
     setIsTabletDropdownOpen(!isTabletDropdownOpen);
+    setIsMobileDropdownOpen(!isTabletDropdownOpen);
   };
 
   const chipsMobile = CHIPS_DATA.slice(0, 5);
@@ -23,7 +25,7 @@ const ChipsCollection = () => {
       <li>
         <ul className="flex flex-wrap">
           {chipsMobile.map((chip) => (
-            <ChipContainer key={chip.id} chip={chip} />
+            <ChipCard key={chip.id} chip={chip} />
           ))}
         </ul>
       </li>
@@ -31,7 +33,7 @@ const ChipsCollection = () => {
       <li className="relative">
         <div
           onClick={handleMobileClick}
-          className="mx-0.5 my-1 rounded-xl bg-neutral-200  px-4 py-2.5 text-xs md:hidden lg:hidden"
+          className="mx-0.5 my-1 cursor-pointer rounded-xl bg-neutral-300 px-4  py-2.5 text-xs hover:bg-neutral-200 md:hidden lg:hidden"
         >
           &#8230;
         </div>
@@ -39,29 +41,29 @@ const ChipsCollection = () => {
           <div className="absolute -left-60 top-12 w-72 rounded px-4  py-2 shadow-md md:hidden lg:hidden">
             <ul className="flex flex-wrap">
               {chipsTablet.map((chip) => (
-                <ChipContainer key={chip.id} chip={chip} />
+                <ChipCard key={chip.id} chip={chip} />
               ))}
             </ul>
             <ul className="flex flex-wrap ">
               {chipsPC.map((chip) => (
-                <ChipContainer key={chip.id} chip={chip} />
+                <ChipCard key={chip.id} chip={chip} />
               ))}
             </ul>
           </div>
         )}
       </li>
 
-      <li className="hidden md:block">
+      <li className="hidden md:block ">
         <ul className="flex flex-wrap  ">
           {chipsTablet.map((chip) => (
-            <ChipContainer key={chip.id} chip={chip} />
+            <ChipCard key={chip.id} chip={chip} />
           ))}
         </ul>
       </li>
       <li className="relative">
         <div
           onClick={handleTabletClick}
-          className="mx-0.5 my-1 hidden rounded-xl bg-neutral-200 px-4 py-2.5 text-xs sm:hidden md:block lg:hidden"
+          className="mx-0.5 my-1 hidden cursor-pointer rounded-xl bg-neutral-300 px-4 py-2.5 text-xs hover:bg-neutral-200 sm:hidden md:block lg:hidden"
         >
           &#8230;
         </div>
@@ -69,7 +71,7 @@ const ChipsCollection = () => {
           <div className="absolute -left-40 top-12 hidden w-48 rounded px-4 py-2 shadow-md md:block lg:hidden">
             <ul className="flex flex-wrap ">
               {chipsPC.map((chip) => (
-                <ChipContainer key={chip.id} chip={chip} />
+                <ChipCard key={chip.id} chip={chip} />
               ))}
             </ul>
           </div>
@@ -79,7 +81,7 @@ const ChipsCollection = () => {
       <li className="hidden lg:block">
         <ul className="flex flex-wrap ">
           {chipsPC.map((chip) => (
-            <ChipContainer key={chip.id} chip={chip} />
+            <ChipCard key={chip.id} chip={chip} />
           ))}
         </ul>
       </li>
