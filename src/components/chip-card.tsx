cdc5fcaa1befe9c6f6577ce79f2703bd1f1a61bc
MaximePeterburg <getmax4444@gmail.com>
2023-10-10 02:11:49 +0300

    added pick a chip functionality

--- src/components/chip-card.tsx
+++ src/components/chip-card.tsx
@@ -7,8 +7,13 @@ type ChipCardProps = {
 
 const ChipCard: FC<ChipCardProps> = ({ chip }) => {
   return (
-    <li className="rounded-xl bg-neutral-200 px-0.5 py-2.5 text-xs">
-      Чипс {chip.id}
+    <li className="inline-block list-none">
+      <label className="cursor-pointer">
+        <input className="peer sr-only" type="checkbox" />
+        <div className="mx-0.5 my-1 rounded-xl bg-neutral-200 px-0.5 py-2.5 text-xs ring-1 ring-transparent peer-checked:ring-indigo-600 peer-checked:ring-offset-1">
+          Чипс {chip.id}
+        </div>
+      </label>
     </li>
   );
 };
